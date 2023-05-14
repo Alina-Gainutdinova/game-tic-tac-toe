90ea7ffc4756af342403b370befe3c4af089cb9f
Azamat Isakov <azamatisoqov5555@gmail.com>
2023-05-14 23:24:53 +0500

    Created func mode_selection and added some info in files index.py and constants.py

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import constans as cns
 def cell_occupancy(k1, k2):
     """
     Вместо переменной 'table' будет переменная в основной программе и будет заменена другой (скорее всего)
@@ -32,3 +33,18 @@ def is_correct_coor(coordinates):
     x, y = coordinates
     if 0 < x < 4 and 0 < y < 4: return True
     else: return False
+
+def mode_selection():
+    while True:
+        print(cns.choose_mode)
+        print(f'1 - {cns.mode_option_1}')
+        print(f'2 - {cns.mode_option_2}')
+        choise = input('>>> ')
+        if choise == '1':
+            pass
+        elif choise == '2':
+            pass
+        else:  
+            print(cns.br)
+            print(cns.wrong_choice_text)
+            print()
--- index.py
+++ index.py
@@ -1,8 +1,14 @@
-import functions
-import constans
+import functions as fn
+import constans as cns
 
 table = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
 ]
+
+print(cns.br)
+print(cns.welcome)
+print(cns.br)
+while True:
+    fn.mode_selection()
